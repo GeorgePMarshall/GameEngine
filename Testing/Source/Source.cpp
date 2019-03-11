8faa664bc6d625c35d5e5f085e5f144225069161
George <George.p.Marshall@gmail.com>
2019-03-12 03:17:30 +1100

    add ability to create window & basic program structure

---
+++ Testing/Source/Source.cpp
@@ -0,0 +1,9 @@
+#include "Testgame.h"
+
+
+
+int main()
+{
+	TestGame game;
+	return game.Run("TestGame");
+}
---
+++ Testing/Source/TestGame.cpp
@@ -0,0 +1,23 @@
+#include "TestGame.h"
+
+
+
+void TestGame::Initialize()
+{
+
+}
+
+void TestGame::Update()
+{
+
+}
+
+void TestGame::Draw()
+{
+
+}
+
+void TestGame::Shutdown()
+{
+
+}
